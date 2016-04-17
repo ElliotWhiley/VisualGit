33a07d339d5a8158e8fd7f2a9ebc4ec7eef8310f
ElliotWhiley <elliot.w@hotmail.com>
2016-04-17 19:37:11 +1200

    Gets commits from specified repo and adds their corresponding branch names

--- src/scripts/github-metrics.js
+++ src/scripts/github-metrics.js
@@ -1,9 +1,14 @@
 'use strict';
+var GitHubApi = require('github');
+var branchNames = [];
+var branches = [];
+var commits;
+var gitUser = 'Puhapig';
+var gitRepo = 'git-history-template';
+var github = new GitHubApi({
+    version: '3.0.0',
+});
 function getFollowers() {
-    var GitHubApi = require('github');
-    var github = new GitHubApi({
-        version: '3.0.0',
-    });
     github.user.getFollowers({
         user: document.getElementById('username').value
     }, function (err, res) {
@@ -16,3 +21,60 @@ function getFollowers() {
         res.forEach(displayFollower);
     });
 }
+document.addEventListener('DOMContentLoaded', function createLabelledCommits() {
+    authenticate();
+    getBranchNames();
+    getCommits();
+    function setBranchName(branch, index, array) {
+        branchNames[index] = branch.name;
+    }
+    function getBranchCommits(branchName, index, array) {
+        github.repos.getCommits({
+            user: gitUser,
+            repo: gitRepo,
+            sha: branchName
+        }, function (err, res) {
+            branches[index] = res;
+        });
+    }
+    function labelCommit(commit, index, array) {
+        for (var i = 0; i < branches.length; i++) {
+            var branch = branches[i];
+            for (var j = 0; j < branch.length; j++) {
+                var branchCommit = branch[j];
+                if (commit.sha === branchCommit.sha) {
+                    commit['branch'] = branchNames[i];
+                    break;
+                }
+            }
+        }
+    }
+    function authenticate() {
+        github.authenticate({
+            type: "basic",
+            username: "EdMinstrateur",
+            password: "somethingsimple123"
+        });
+    }
+    function getBranchNames() {
+        github.repos.getBranches({
+            user: gitUser,
+            repo: gitRepo
+        }, function (err, res) {
+            res.forEach(setBranchName);
+            branchNames.forEach(getBranchCommits);
+        });
+    }
+    function getCommits() {
+        setTimeout(function () {
+            github.repos.getCommits({
+                user: gitUser,
+                repo: gitRepo
+            }, function (err, res) {
+                commits = res;
+                commits.forEach(labelCommit);
+                console.log(commits);
+            });
+        }, 5000);
+    }
+}, false);
--- src/scripts/github-metrics.ts
+++ src/scripts/github-metrics.ts
@@ -1,42 +1,102 @@
 'use strict';
 
-function getFollowers() {
-  // GitHub API trial 2
-  var GitHubApi = require('github');
+var GitHubApi = require('github');
 
-  var github = new GitHubApi({
-    // required
-    version: '3.0.0',
-    // // optional
-    // debug: true,
-    // protocol: "https",
-    // host: "github.my-GHE-enabled-company.com", // should be api.github.com for GitHub
-    // pathPrefix: "/api/v3", // for some GHEs; none for GitHub
-    // timeout: 5000,
-    // headers: {
-    //     "user-agent": "My-Cool-GitHub-App" // GitHub is happy with a unique user agent
-    // }
-  });
-  //github.user.getFollowingFromUser({
+var branchNames = [];
+var branches = [];
+var commits;
+var gitUser = 'Puhapig';
+var gitRepo = 'git-history-template';
+
+var github = new GitHubApi({
+  version: '3.0.0',
+});
+
+function getFollowers() {
+
   github.user.getFollowers({
-      // optional:
-      // headers: {
-      //     "cookie": "blahblah"
-      // },
-      //user: "mikedeboer"
       user: document.getElementById('username').value
   }, function(err, res) {
         console.log(JSON.stringify(res));
-
       document.getElementById('numFollowers').innerHTML = 'Number of followers: ' + res.length;
-
       document.getElementById('followers').innerHTML = 'Follower names: ';
-      // loop through all followers and display their names
+      //Search through all followers and display their names
       function displayFollower(follower, index, array) {
         document.getElementById('followers').innerHTML += follower.login + ', ';
       }
-
       res.forEach(displayFollower);
-
   });
 }
+
+document.addEventListener( 'DOMContentLoaded', function createLabelledCommits() {
+  //Get latest _____ commits from specified repo and label each with its corresponding branch
+  authenticate();
+  getBranchNames();
+  getCommits();
+
+  function setBranchName(branch, index, array) {
+    branchNames[index] = branch.name;
+  }
+
+  function getBranchCommits(branchName, index, array) {
+    github.repos.getCommits({
+      user: gitUser,
+      repo: gitRepo,
+      sha: branchName
+    }, function(err, res) {
+      branches[index] = res;
+    });
+  }
+
+  function labelCommit(commit, index, array) {
+      for (var i = 0; i < branches.length; i++) {
+        var branch = branches[i];
+        for (var j = 0; j < branch.length; j++) {
+          var branchCommit = branch[j];
+          if (commit.sha === branchCommit.sha) {
+            commit['branch'] = branchNames[i];
+            break;
+          }
+        }
+  }
+}
+
+function authenticate() {
+  github.authenticate({
+    type: "basic",
+    username: "EdMinstrateur",
+    password: "somethingsimple123"
+  });
+}
+
+function getBranchNames() {
+  //Get all branch names from repo
+  github.repos.getBranches({
+    user: gitUser,
+    repo: gitRepo
+  }, function(err, res) {
+    //Set branch names
+    res.forEach(setBranchName);
+    //Get commits for each branch
+    branchNames.forEach(getBranchCommits);
+  });
+}
+
+function getCommits() {
+  setTimeout(function(){
+    //Get all commits from repo
+    github.repos.getCommits({
+      user: gitUser,
+      repo: gitRepo
+    }, function(err, res) {
+      commits = res;
+      //Label each commit with a branch name
+      commits.forEach(labelCommit);
+      console.log(commits);
+    });
+  }, 5000);
+}
+
+
+
+, false);
