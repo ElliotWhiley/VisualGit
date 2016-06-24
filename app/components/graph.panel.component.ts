a4e1029f14221e25593b10fb08e393e1931c313e
elliotwhiley <elliot.whiley94@gmail.com>
2016-06-24 13:53:29 +1200

    Added IDs to file panel and graph panel so they can be accessed via DOM

--- app/components/graph.panel.component.ts
+++ app/components/graph.panel.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'graph-panel',
   template: `
-  <div class="graph-panel">
+  <div class="graph-panel" id="graph-panel">
     <div class="network" id="my-network">
     </div>
   </div>
