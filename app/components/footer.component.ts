2a37f190ad2be8a2098eaeb7bd74f756521b790c
elliotwhiley <elliot.whiley94@gmail.com>
2016-09-04 17:43:44 +1200

    Changed footer to accomodate git commands'

--- app/components/footer.component.ts
+++ app/components/footer.component.ts
@@ -3,11 +3,7 @@ import { Component } from "@angular/core";
 @Component({
   selector: "app-footer",
   template: `
-  <div class="footer">
-    <p>Git Commands</p>
-    <div class="git-command">
-      <p id="git-command">test</p>
-    </div>
+  <div class="footer" id="footer">
   </div>
   `
 })
