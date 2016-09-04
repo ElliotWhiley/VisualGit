f3589594cff80c7d2b090a2c0cd3dbd22ff58a4d
elliotwhiley <elliot.whiley94@gmail.com>
2016-09-04 23:30:17 +1200

    Added back button to add repository panel

--- app/components/add.repository.component.ts
+++ app/components/add.repository.component.ts
@@ -4,41 +4,45 @@ import { Component } from "@angular/core";
   selector: "add-repository-panel",
   template: `
     <div class="add-repository-panel" id="add-repository-panel">
-      <div class=title>
-        <h1>Clone from internet</h1>
-      </div>
+      <img src="./assets/Back.svg" (click)="returnToMainPanel()" class="back-button">
 
-      <div class=block>
-        <div class=left>
-          <p>URL to clone from</p>
+      <div class="add-repository-body">
+        <div class="title">
+          <h1 class="clone-title">Clone from Internet</h1>
         </div>
-        <div class=right>
-        <input type="text" oninput="updateLocalPath()" name="repositoryRemote" size="50" id="repoClone" placeholder="https://github.com/user/repository.git"/>
-        </div>
-      </div>
 
-      <div class=block>
-        <div class=left>
-          <p>File location to save to</p>
+        <div class="block">
+          <div class="left">
+            <p>URL to clone from</p>
+          </div>
+          <div class="right">
+          <input type="text" oninput="updateLocalPath()" name="repositoryRemote" size="50" id="repoClone" placeholder="https://github.com/user/repository.git"/>
+          </div>
         </div>
-        <div class=right>
-          <input type="text" name="repositoryLocal" size="50" id="repoSave"/>
-          <button class="button-clone" (click)="addRepository()">Clone</button>
+
+        <div class="block">
+          <div class="left">
+            <p>File location to save to</p>
+          </div>
+          <div class="right">
+            <input type="text" name="repositoryLocal" size="50" id="repoSave"/>
+            <button class="button-clone" (click)="addRepository()">Clone</button>
+          </div>
         </div>
-      </div>
 
 
-      <div class=title>
-        <h1 class="open-local-repo">Open local repository</h1>
-      </div>
-
-      <div class=block>
-        <div class=left>
-          <p>Location of existing repository</p>
+        <div class="title">
+          <h1 class="open-local-repo">Open Local Repository</h1>
         </div>
-        <div class=right>
-          <input type="text" name="repositoryLocal" size="50" id="repoOpen"/>
-          <button class="button-open" (click)="openRepository()">Open</button>
+
+        <div class="block">
+          <div class="left">
+            <p>Location of existing repository</p>
+          </div>
+          <div class="right">
+            <input type="text" name="repositoryLocal" size="50" id="repoOpen"/>
+            <button class="button-open" (click)="openRepository()">Open</button>
+          </div>
         </div>
       </div>
     </div>
@@ -54,6 +58,10 @@ export class AddRepositoryComponent {
 
   openRepository(): void {
     openRepository();
+    switchToMainPanel();
+  }
+
+  returnToMainPanel(): void {
     switchToMainPanel();
   }
 }
